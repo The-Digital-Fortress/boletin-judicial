bb36357f24aa4f7d65930012b1dbe92533711174
Arturo Mendivil <arturoo1516@gmail.com>
2023-05-22 21:33:09 -0700

    fix navbar,favicon,text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,26 @@
+import type { LinksFunction } from "@remix-run/node";
 import type { V2_MetaFunction } from "@remix-run/react";
-import Example from "~/components/Navbar";
+import Navbar from "~/components/Navbar";
 import Hero from "~/components/Hero";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "Expediente Legal" }];
 };
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "icon",
+      href: "https://user-images.githubusercontent.com/60411196/240128272-722a7af9-a5e7-4ae5-8d16-bbb4b650668f.png",
+      type: "image/png",
+    },
+  ];
+}
 
 export default function Index() {
   return (
     <div className="flex flex-col h-screen">
-      <Example />
+      <Navbar />
       <Hero />
     </div >
   );
