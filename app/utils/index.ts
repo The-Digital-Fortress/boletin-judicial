2bd51208cee5b072c559f330fa5aef86d71733af
Hector Garcia <48266696+hectorg2211@users.noreply.github.com>
2023-08-29 15:32:25 -0700

    Add filter and sort (#68)
    * ⭐ Add reducer for resumen section
    
    * ⭐ Add sort
    
    * ⭐ Add search
    
    * 🧰 Remove log
    
    * ⭐ Remove edit button

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -4,7 +4,7 @@ export function classNames(...classes: any[]) {
 
 export function convertDateToLocale(date: string) {
   if (!date) return null
-  
+
   const newDate = new Date(date)
   const formattedDate = newDate.toLocaleDateString('es-MX', {
     day: 'numeric',
@@ -14,4 +14,34 @@ export function convertDateToLocale(date: string) {
 
   if (formattedDate === 'Invalid Date') return null
   return formattedDate
+}
+
+const compareAscending = (dateStr1: string, dateStr2: string) => {
+  const date1 = new Date(dateStr1)
+  const date2 = new Date(dateStr2)
+
+  if (date1 < date2) return -1
+  if (date1 > date2) return 1
+  return 0
+}
+
+const compareDescending = (dateStr1: string, dateStr2: string) => {
+  const date1 = new Date(dateStr1)
+  const date2 = new Date(dateStr2)
+
+  if (date1 < date2) return 1
+  if (date1 > date2) return -1
+  return 0
+}
+
+export function compareDates(dateStr1: string, dateStr2: string, sort: string) {
+  if (sort === 'oldest') {
+    return compareAscending(dateStr1, dateStr2)
+  } else {
+    return compareDescending(dateStr1, dateStr2)
+  }
+}
+
+export function compareStrings(string1: string, string2: string, sort: string) {
+  return string1.localeCompare(string2)
 }
