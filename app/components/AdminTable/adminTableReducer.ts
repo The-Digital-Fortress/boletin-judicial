f9b9c78a59031e1e65d0ffd29c4653562fe117bb
Hector Garcia <48266696+hectorg2211@users.noreply.github.com>
2023-09-18 11:22:00 -0700

    ⭐ Add confirmation window (#71)

--- app/components/AdminTable/adminTableReducer.ts
+++ app/components/AdminTable/adminTableReducer.ts
@@ -1,15 +1,27 @@
 export const actionTypes = {
   SET_MODAL_OPEN: 'SET_MODAL_OPEN',
+  SET_CONFIRMATION_WINDOW_OPEN: 'SET_CONFIRMATION_WINDOW_OPEN',
+  SET_SELECTED_FILES: 'SET_SELECTED_FILES',
+  SET_INDETERMINATE: 'SET_INDETERMINATE',
 }
 
 export const initialState = {
   modalOpen: false,
+  confirmationWindowOpen: false,
+  selectedFiles: [],
+  indeterminate: false,
 }
 
 export function adminTableReducer(state, action) {
   switch (action.type) {
     case 'SET_MODAL_OPEN':
       return { ...state, modalOpen: action.payload }
+    case 'SET_CONFIRMATION_WINDOW_OPEN':
+      return { ...state, confirmationWindowOpen: action.payload }
+    case 'SET_SELECTED_FILES':
+      return { ...state, selectedFiles: action.payload }
+    case 'SET_INDETERMINATE':
+      return { ...state, indeterminate: action.payload }
     default:
       return state
   }
