23d9f26fa7e5214bf27f56d9ff245e3b69e7f87d
Arturo Mendivil <arturoo1516@gmail.com>
2024-01-17 18:34:50 -0800

    revert netlify.toml

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? './server.js'
       : undefined,
-  // serverBuildPath: '.netlify/functions-internal/server.js',
+  serverBuildPath: '.netlify/functions-internal/server.js',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
